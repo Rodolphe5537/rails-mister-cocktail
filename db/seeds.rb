941d1a1f69c4ddda4cf68234c495318c322080bd
Rodolphe5537 <rodolphe@dedorlodot.net>
2019-03-01 15:50:52 +0100

    en cours3

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.destroy_all
+Cocktail.destroy_all
+
 Ingredient.create(name: "lemon")
 Ingredient.create(name: "ice")
 Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "bloody mary")
+Cocktail.create(name: "mojito")
+Cocktail.create(name: "sex on the beach")
